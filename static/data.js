092eb68b170b81091d4cb6b5bcf7ba4c7e16fd3c
admin <admin@Ohene-Mills.local>
2023-08-09 12:26:00 +0000

    new push

---
+++ static/data.js
@@ -0,0 +1,50 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: 'static/musics/Song 1.mp3',
+        artist: 'artist 1',
+        cover: 'static/images/cover 1.png'
+    },
+    {
+        name: 'song 2',
+        path: 'static/musics/Song 2.mp3',
+        artist: 'artist 2',
+        cover: 'static/images/cover 2.png'
+    },
+    {
+        name: 'song 3',
+        path: 'static/musics/Song 3.mp3',
+        artist: 'artist 3',
+        cover: 'static/images/cover 3.png'
+    },
+    {
+        name: 'song 4',
+        path: 'static/musics/Song 4.mp3',
+        artist: 'artist 4',
+        cover: 'static/images/cover 4.png'
+    },
+    {
+        name: 'song 5',
+        path: 'static/musics/Song 5.mp3',
+        artist: 'artist 5',
+        cover: 'static/images/cover 5.png'
+    },
+    {
+        name: 'song 6',
+        path: 'static/musics/Song 6.mp3',
+        artist: 'artist 6',
+        cover: 'static/images/cover 6.png'
+    },
+    {
+        name: 'song 7',
+        path: 'static/musics/Song 7.mp3',
+        artist: 'artist 7',
+        cover: 'static/images/cover 7.png'
+    },
+    {
+        name: 'song 8',
+        path: 'static/musics/Song 8.mp3',
+        artist: 'artist 8',
+        cover: 'static/images/cover 8.png'
+    },
+]
